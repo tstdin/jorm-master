0b51a1178fa6858293c706347575861f8a3ec5e2
Tomáš Stefan <ts@stdin.cz>
2020-01-20 17:38:17 +0100

    runner's storage on disk

--- jorm_runner.sh
+++ jorm_runner.sh
@@ -4,11 +4,12 @@
 CFG_PATH=/etc/cardano
 CONFIG=jorm_runner.yaml
 SECRET=node_secret.yaml
+STORAGE="/home/cardano/storage_jorm_runner"
 
 export RUST_BACKTRACE=full
 
 exec jormungandr --genesis-block-hash $(< /etc/cardano/genesis.txt) \
                  --config "${CFG_PATH}/${CONFIG}"                   \
-                 --rest-listen "127.0.0.1:310$1"                    \
-                 --storage "/tmp/jormungandr_runner_$1"             \
+                 --rest-listen "127.0.0.1:310${1}"                  \
+                 --storage "${STORAGE}_${1}"                        \
                  --secret "${CFG_PATH}/${SECRET}"
