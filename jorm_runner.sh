d8a331d2eb78b88204e52983cc83e25b8270f75a
Tomáš Stefan <ts@stdin.cz>
2020-01-20 19:55:33 +0100

    Cleanup the jorm_runner exec script

--- jorm_runner.sh
+++ jorm_runner.sh
@@ -1,9 +1,6 @@
 #!/bin/bash
 # Startup script for Jormungandr runner
 
-SECRET="/etc/cardano/node_secret.yaml"
-STORAGE="/home/cardano/storage_jorm_runner"
-
 export RUST_BACKTRACE=full
 
 exec jormungandr --genesis-block-hash $(< /etc/cardano/genesis.txt) \
