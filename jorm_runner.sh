5be9840ac0da48d19226a4efe7eccdea33d6d77e
Tomáš Stefan <ts@stdin.cz>
2020-01-20 17:45:18 +0100

    Separate configs

--- jorm_runner.sh
+++ jorm_runner.sh
@@ -1,15 +1,13 @@
 #!/bin/bash
 # Startup script for Jormungandr runner
 
-CFG_PATH=/etc/cardano
-CONFIG=jorm_runner.yaml
-SECRET=node_secret.yaml
+SECRET="/etc/cardano/node_secret.yaml"
 STORAGE="/home/cardano/storage_jorm_runner"
 
 export RUST_BACKTRACE=full
 
 exec jormungandr --genesis-block-hash $(< /etc/cardano/genesis.txt) \
-                 --config "${CFG_PATH}/${CONFIG}"                   \
+                 --config "/etc/cardano/jorm_runner_${1}.yaml"      \
                  --rest-listen "127.0.0.1:310${1}"                  \
-                 --storage "${STORAGE}_${1}"                        \
-                 --secret "${CFG_PATH}/${SECRET}"
+                 --storage "/home/cardano/storage_jorm_runner_${1}" \
+                 --secret "/etc/cardano/node_secret.yaml"
